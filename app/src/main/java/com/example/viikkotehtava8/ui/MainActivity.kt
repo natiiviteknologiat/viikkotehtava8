550c74cb0e65932c73616569af748218ed0c8e20
Rapuska <118633578+Rapuska@users.noreply.github.com>
2024-10-13 20:24:46 +0300

    part 1 viikkotehtava8 done

---
+++ app/src/main/java/com/example/viikkotehtava8/ui/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.viikkotehtava8.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.viikkotehtava8.ui.theme.Viikkotehtava8Theme
+import com.example.viikkotehtava8.viewmodel.TodoViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Viikkotehtava8Theme {
+                TodoScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoScreen(todoViewModel: TodoViewModel = viewModel()) {
+    TodoList(todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<String>) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(todos) { todo ->
+            Text(
+                text = todo,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TodoScreenPreview() {
+    Viikkotehtava8Theme {
+        TodoScreen()
+    }
+}
